3c1aac4d010a9df71312a22e9a4057a012ecae91
Ammar Ahmed <ammarejaz1874@gmail.com>
2021-07-26 23:07:39 +0500

    Update import.tf

--- task1/import.tf
+++ task1/import.tf
@@ -3,14 +3,15 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {}
+resource "azurerm_storage_account" "example" {}
 
-resource "azurerm_virtual_network" "example" {}
+#resource "azurerm_virtual_network" "example" {}
 
-resource "azurerm_linux_virtual_machine" "example" {}
+#resource "azurerm_linux_virtual_machine" "example" {}
 
-resource "azurerm_public_ip" "myterraformpublicip" {}
+#resource "azurerm_public_ip" "myterraformpublicip" {}
 
-resource "azurerm_network_security_group" "myterraformnsg" {}
+#resource "azurerm_network_security_group" "myterraformnsg" {}
 
-resource "azurerm_network_interface" "myterraformnic" {}
+#resource "azurerm_network_interface" "myterraformnic" {}
 
